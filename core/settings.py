ec9365e654c9ebc12192957b1e5b1abe23050a70
aldoGuerra <61807769+aldoGuerra@users.noreply.github.com>
2021-06-23 19:44:32 -0500

    Update settings.py
    Update database password

--- core/settings.py
+++ core/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': 'psql&$4n4l1s1s',
+        'PASSWORD': 'postgres',
         'HOST': 'pgdb',
         'PORT': 15432,
     }
